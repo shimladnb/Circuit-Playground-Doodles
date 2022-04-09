da05715b8569a7028417d7bb23e01c69d8a6ddf1
Sem <semschreuder@live.nl>
2022-04-09 15:08:14 +0200

    change hue with motion

--- NEOP_Accelerometer.ino
+++ NEOP_Accelerometer.ino
@@ -2,6 +2,7 @@
 // http://ermahgerd.info/Adafruit_CircuitPlayground/
 #include <Smoothed.h>
 #include <FastLED.h>
+// https://github.com/FastLED/FastLED/wiki/Pixel-reference#chsv
 
 float X, Y, Z;
 int counter;
@@ -10,6 +11,7 @@ int smoothAmt = 20;
 int ledBrightness = 255;
 float oldValX, oldValY, oldValZ;
 float deltaX, deltaY, deltaZ;
+int initialHue = 80;
 
 Smoothed <float> smoothyX, smoothyY, smoothyZ, smoothDeltaX, smoothDeltaY, smoothDeltaZ, smoothMotion;
 
@@ -35,24 +37,22 @@ void loop()
 {
   prepAccels(false);
   setBrightnessWithButton(false);
-
-  for (int i = 0; i < 10; i++)
-  {
-    static uint8_t hue;
-    hue = hue + 1;
-    setColorToPixel(i, CHSV( hue, 255, 255));
-    
-  }
-
   calculateDeltaVector();
-  
+
   float generalMotion = abs(deltaX + deltaY + deltaZ);
   smoothMotion.add(generalMotion);
   generalMotion = smoothMotion.get();
   generalMotion /= 32.0;
   generalMotion = pow(generalMotion, 2);
-  Serial.println(generalMotion*100);
+  Serial.println(generalMotion * 100);
   CircuitPlayground.setBrightness(ledBrightness * generalMotion);
+
+  for (int i = 0; i < 10; i++)
+  {
+    static uint8_t hue;
+    hue = hue + 1;
+    setColorToPixel(i, CHSV(initialHue + (generalMotion * 100), 255, 255));
+  }
 
   delay(10);
 }
